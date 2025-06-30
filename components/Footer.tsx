3bfb63866a0d5cfe3ab56a8a29c3ad2a59f6c4a2
Lucas Gomes <lucas.cgomesb19@gmail.com>
2025-06-30 16:34:19 -0300

    update h w photo footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -15,7 +15,7 @@ export default function Footer() {
               <img
                 src="/carlos-foto.png"
                 alt="Carlos Alberto"
-                className="w-32 h-32 rounded-full shadow-lg object-cover"
+                className="w-42 h-42 rounded-full shadow-lg object-cover"
               />
               <img src="/carlos-alberto.png" alt="Carlos Alberto" className="h-8 w-auto max-w-xs" />
               <img src="/creci.png" alt="CRECI 176.642F" className="h-6 w-auto max-w-xs" />
